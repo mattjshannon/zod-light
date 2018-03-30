e609580753d2ad0d7aaa5818bf53d976bdf7fc7b
Matt Shannon <matthew.j.shannon@gmail.com>
2018-03-30 12:40:07 -0700

    Another conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['matplotlib', 'numpy', 'astropy', 'astropy.coordinates', 'concurrent.futures']
+MOCK_MODULES = ['matplotlib', 'numpy', 'astropy', 'astropy.coordinates', 'concurrent', 'concurrent.futures']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
