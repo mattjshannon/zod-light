8a2cc54e0a12c0cc59680cd19a645f744ca6907d
Matt Shannon <matthew.j.shannon@gmail.com>
2018-03-30 11:01:01 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zod-light',
+    version='0.1dev',
+    author='Matthew J. Shannon',
+    author_email='matthew.j.shannon@gmail.com',
+    packages=['zod-light'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
