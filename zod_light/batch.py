dd50c3d3c468941e90b2c5022d07a3fc5c9a3fec
Matthew J. Shannon <matthew.j.shannon@gmail.com>
2018-10-02 14:26:27 -0700

    Added batch.py to process Spitzer .tbl files; ran on dataset.

---
+++ zod_light/batch.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""
+batch.py
+
+Batch run of zod_light.py.
+"""
+
+import glob
+import numpy as np
+
+from datetime import date
+from ipdb import set_trace as st
+from zod_light import Observation
+
+
+def extract_observation_properties(table):
+    """Extract the RA, Dec and date information from the .tbl file."""
+
+    def prep_label(table):
+        """Extract a unique identifier."""
+        label = table.split('/')[-1]
+
+        return label
+
+    def read_table(table):
+        # Isolate the lines containing the date and coordinate information.
+        with open(table) as f:
+            for line in f:
+                if line[:9] == '\\DATE_OBS':
+                    date_str = line
+                elif line[:9] == '\\CEN_SL1 ':
+                    radec_str = line
+                elif line[:9] == '\\CEN_SL2 ':
+                    radec_str = line
+                elif line[:9] == '\\CEN_SL3 ':
+                    radec_str = line
+
+        return date_str, radec_str
+
+    def prep_date_str(date_str):
+        """Process the date_str to number of days, year for zod-light."""
+
+        # Extract the year, month, day of observation.
+        ymd_str = date_str.split('\'')[1].split('T')[0]
+        year, month, day = [int(x) for x in ymd_str.split('-')]
+
+        # Compute the number of days since Jan. 1st of that year.
+        obs_date = date(year, month, day)
+        ref_date = date(year, 1, 1)
+        days = (obs_date - ref_date).days
+
+        return days, year
+
+    def prep_radec_str(radec_str):
+        """Extract the RA, Dec from radec_str in the right style."""
+
+        # Isolate the part of the line that contains the info we need.
+        sub_str = radec_str.split('\'')[1].split(' ')
+
+        # Extract individual components.
+        ra_hour = sub_str[0]
+        ra_min = sub_str[1]
+        ra_sec = sub_str[2]
+
+        dec_hour = sub_str[4]
+        dec_min = sub_str[5]
+        dec_sec = sub_str[6]
+
+        # Combine in the desired format.
+        ra = ra_hour + ':' + ra_min + ':' + ra_sec
+        dec = dec_hour + ':' + dec_min + ':' + dec_sec
+
+        return ra, dec
+
+    # Extract label.
+    label = prep_label(table)
+
+    # Extract the necessary lines.
+    date_str, radec_str = read_table(table)
+
+    # Parse the date information.
+    days, year = prep_date_str(date_str)
+
+    # Parse the RA, Dec information.
+    ra, dec = prep_radec_str(radec_str)
+
+    return label, ra, dec, days, year
+
+
+def main():
+
+    # Desired output spectrum parameters.
+    wave_low = 5  # microns
+    wave_high = 15  # microns
+    wave_step = 0.1  # microns
+    wave_arr = np.arange(wave_low, wave_high, wave_step)
+
+    # Identify all input files.
+    table_list = np.sort(glob.glob('../inputs/tbl/*_nd.tbl'))
+    err_list = []
+
+    # Create zodiacal estimate for all tables.
+    for index, table in enumerate(table_list):
+
+        # Extract the meta information.
+        try:
+            label, ra, dec, days, year = extract_observation_properties(table)
+        except Exception as e:
+            print(e)
+            err_list.append(e)
+            continue
+
+        # Run!
+        obs = Observation(label, ra, dec, days, year,
+                          wave=wave_arr, verbose=False)
+        obs.construct_spectrum(parallelize=True)
+        obs.write_to_file()
+
+    # Print the error list.
+    print(err_list)
+
+    st()
+
+
+if __name__ == '__main__':
+    main()
--- zod_light/zod_light.py
+++ zod_light/zod_light.py
@@ -21,7 +21,7 @@ class Observation(object):
     e.g., C32-1.
     """
 
-    def __init__(self, label, ra, dec, days, year, wave=None):
+    def __init__(self, label, ra, dec, days, year, wave=None, verbose=False):
         """Initialize an instance of class Observation.
 
         Note:
@@ -32,7 +32,7 @@ class Observation(object):
             dec (str): HMS string of J2000 declination.
             days (int): Number of days since Jan 1st of corresponding year.
             year (int): Year of observation.
-            wave (numpy.ndarray), optional: Desired output spectrum wavelengths.
+            wave (numpy.ndarray), optional: Desired output spectrum wavelength.
         """
         self.label = label
         self.ra = ra
@@ -44,11 +44,13 @@ class Observation(object):
         if wave is not None:
             self.wave_array = wave
         else:
-            self.wave_array = np.range(5, 40, 0.05) # default
+            self.wave_array = np.range(5, 40, 0.05)  # default
 
-        self._run_info()
-        self.zody_array = self.construct_spectrum()            
-        self.write_to_file(self.label + '.txt', directory='output_spectra/')
+        if verbose:
+            self._run_info()
+
+        # self.zody_array = self.construct_spectrum()
+        # self.write_to_file(self.label + '.txt', directory='../outputs/')
 
     def _run_info(self):
         print()
@@ -60,13 +62,13 @@ class Observation(object):
 
     def to_decimal(self):
         """Convert the RA/DEC into decimal form from HMS.
-        
+
         Returns:
             ra_decimal (str): J2000 RA in decimal form.
             dec_decimal (str): J2000 declination in decimal form.
         """
         # convert ra and dec strings into decimals here.
-        
+
         coords = SkyCoord(self.ra + " " + self.dec, unit=(u.hourangle, u.deg))
         return coords.ra.degree, coords.dec.degree
 
@@ -94,7 +96,7 @@ class Observation(object):
         """
 
         # CD ???
-        os.chdir("../model")
+        os.chdir("../model/")
 
         run_list = ['./BackgroundModel',
                     str(self.ra_decimal), str(self.dec_decimal),
@@ -103,7 +105,7 @@ class Observation(object):
         result = subprocess.run(run_list, stdout=subprocess.PIPE)
 
         # CD BACK???
-        os.chdir("../zod-light")
+        os.chdir("../zod_light/")
 
         return result.stdout
 
@@ -124,7 +126,8 @@ class Observation(object):
         """Construct the specturm by iterating over wavelengths.
 
         Args:
-            parallelize (bool), optional: Whether to parallelize the computation.
+            parallelize (bool), optional: Whether to parallelize the
+                computation.
 
         Returns:
             wave_array (np.ndarray): Wavelengths.
@@ -138,11 +141,14 @@ class Observation(object):
 
         else:
             pool = ProcessPoolExecutor(max_workers=4)
-            zody_array = list(pool.map(self.estimate_zodiacal_light, wave_array))
+            zody_array = \
+                list(pool.map(self.estimate_zodiacal_light, wave_array))
 
-        return zody_array
+        self.zody_array = np.array(zody_array)
 
-    def write_to_file(self, filename, directory=''):
+        return
+
+    def write_to_file(self, filename=None, directory=''):
         """Write wave_array and zody_array to a file!
 
         Args:
@@ -154,8 +160,12 @@ class Observation(object):
         """
         save_array = np.vstack((self.wave_array, self.zody_array)).T
 
-        my_header = 'wavelength (micron), zodiaical light estimate (MJy/sr)'
-        np.savetxt(directory + filename, save_array, delimiter=',', header=my_header)
+        directory = '../outputs/'
+        filename = self.label + '.zod'
+
+        my_header = 'Wavelength (micron), Zodiacal light estimate (MJy/sr)'
+        np.savetxt(directory + filename, save_array, delimiter=',',
+                   header=my_header)
         print("Spectrum saved: ", directory + filename)
 
         return True
@@ -165,71 +175,15 @@ def main():
     print("Starting timer...")
     start_all = time.time()
 
-
     # Desired output spectrum parameters.
     wave_low = 5  # microns
-    wave_high = 40  # microns
+    wave_high = 15  # microns
     wave_step = 0.1  # microns
 
     wave_arr = np.arange(wave_low, wave_high, wave_step)
 
     Observation("test_observation", "17:41:17.50", "-28:29:57.5", 109, 2005,
-                wave=wave_arr)#, plot=True)
-
-    # Observation("C32_1", "17:41:17.50", "-28:29:57.5", 109, 2005)
-    # Observation("C32_2", "17:41:22.70", "-28:31:47.00", 109, 2005)
-    # Observation("C32_3", "17:41:23.56", "-28:27:24.20", 109, 2005)
-    # Observation("C32_4", "17:41:26.60", "-28:27:02.20", 109, 2005)
-    # Observation("C32_5", "17:41:27.26", "-28:28:52.10", 109, 2005)
-    # Observation("C32_6", "17:41:27.88", "-28:28:17.10", 109, 2005)
-    # Observation("C32_7", "17:41:28.51", "-28:27:33.80", 109, 2005)
-    # Observation("C32_8", "17:41:30.15", "-28:28:01.30", 109, 2005)
-    # Observation("C32_9", "17:41:34.60", "-28:24:31.40", 109, 2005)
-    # Observation("C32_10", "17:41:39.48", "-28:24:28.20", 109, 2005)
-    # Observation("C32_11", "17:41:39.94", "-28:25:21.20", 109, 2005)
-    # Observation("C32_12", "17:41:55.27", "-28:16:38.70", 109, 2005)
-    # Observation("C32_13", "17:41:57.53", "-28:22:37.70", 109, 2005)
-    # Observation("C32_14", "17:41:58.73", "-28:18:49.20", 109, 2005)
-    # Observation("C32_15", "17:42:03.69", "-28:17:29.90", 109, 2005)
-    # Observation("C32_16", "17:42:06.86", "-28:18:32.40", 109, 2005)
-
-    # Observation("C35_1", "17:49:16.96", "-29:35:02.70", 109, 2005)
-    # Observation("C35_2", "17:49:23.99", "-29:35:22.20", 109, 2005)
-    # Observation("C35_3", "17:49:43.65", "-29:21:54.50", 109, 2005)
-    # Observation("C35_4", "17:49:48.05", "-29:21:04.80", 109, 2005)
-    # Observation("C35_5", "17:49:51.65", "-29:21:08.70", 109, 2005)
-
-    # Observation("OGLE_1", "17:54:31.94", "-29:53:26.50", 109, 2005)
-    # Observation("OGLE_2", "17:54:56.80", "-29:41:57.40", 109, 2005)
-    # Observation("OGLE_3", "17:54:58.98", "-29:47:01.40", 109, 2005)
-    # Observation("OGLE_4", "17:55:11.90", "-29:40:27.80", 109, 2005)
-    # Observation("OGLE_5", "17:55:15.41", "-29:41:22.80", 109, 2005)
-    # Observation("OGLE_6", "17:55:16.97", "-29:41:31.90", 109, 2005)
-    # Observation("OGLE_7", "17:55:21.70", "-29:39:13.00", 109, 2005)
-
-    # Observation("NGC6522_1", "18:02:34.78", "-29:59:58.90", 104, 2005)
-    # Observation("NGC6522_2", "18:02:38.72", "-29:59:54.60", 104, 2005)
-    # Observation("NGC6522_3", "18:02:48.90", "-29:54:31.00", 249, 2005)
-    # Observation("NGC6522_4", "18:02:49.44", "-29:58:53.40", 249, 2005)
-    # Observation("NGC6522_5", "18:02:59.51", "-30:02:54.30", 104, 2005)
-    # Observation("NGC6522_6", "18:03:01.60", "-30:00:01.10", 249, 2005)
-    # Observation("NGC6522_7", "18:03:04.80", "-29:52:59.30", 249, 2005)
-    # Observation("NGC6522_8", "18:03:05.25", "-29:55:15.90", 104, 2005)
-    # Observation("NGC6522_9", "18:03:05.33", "-29:55:27.80", 249, 2005)
-    # Observation("NGC6522_10", "18:03:08.11", "-29:57:48.00", 249, 2005)
-    # Observation("NGC6522_11", "18:03:08.52", "-30:05:26.50", 104, 2005)
-    # Observation("NGC6522_12", "18:03:08.69", "-29:52:20.40", 104, 2005)
-    # Observation("NGC6522_13", "18:03:11.47", "-29:57:47.20", 104, 2005)
-    # Observation("NGC6522_14", "18:03:13.88", "-29:56:20.90", 249, 2005)
-    # Observation("NGC6522_15", "18:03:15.99", "-29:55:38.30", 249, 2005)
-    # Observation("NGC6522_16", "18:03:23.84", "-29:54:10.70", 249, 2005)
-    # Observation("NGC6522_17", "18:03:28.36", "-29:55:45.40", 104, 2005)
-    # Observation("NGC6522_18", "18:03:33.26", "-29:59:11.50", 249, 2005)
-    # Observation("NGC6522_19", "18:03:34.07", "-29:59:58.80", 104, 2005)
-
-    # Observation("NEP_C32C35OGLE", "18:00:00.00", "+66:33:38.55", 109, 2005)
-    # Observation("NEP_NGC6522_a", "18:00:00.00", "+66:33:38.55", 104, 2005)
-    # Observation("NEP_NGC6522_bc", "18:00:00.00", "+66:33:38.55", 249, 2005)
+                wave=wave_arr)  # , plot=True)
 
     elapsed = (time.time() - start_all)
     print("Elapsed time (s): ", elapsed)
